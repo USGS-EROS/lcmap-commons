5088a729e1b9011316da0165f1ef14272ce4eb47
David Hill <davehill75@gmail.com>
2016-09-08 18:55:57 -0500

    correcting formatting

--- test/lcmap/commons/time_test.clj
+++ test/lcmap/commons/time_test.clj
@@ -4,8 +4,8 @@
             [clj-time.core :as t]))
 
 (deftest timestamp-test
-   (testing "lcmap.commons.time timestamps"
-       (let [ts (t/now)]
-           (is (= ts
-                  (str->timestamp
-                      (timestamp->str ts)))))))
+  (testing "lcmap.commons.time timestamps"
+    (let [ts (t/now)]
+      (is (= ts
+             (str->timestamp
+              (timestamp->str ts)))))))
