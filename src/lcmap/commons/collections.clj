160d2346451659e46ef54b561c8e7d360a994dae
davidvhill <davehill75@gmail.com>
2017-01-06 13:39:10 -0600

    replacing vectorize implementation

--- src/lcmap/commons/collections.clj
+++ src/lcmap/commons/collections.clj
@@ -1,10 +1,8 @@
 (ns lcmap.commons.collections)
 
 (defn vectorize
-  "Guarantees value is a vector."
+  "Guarantees value is a vector"
   [value]
-  (cond (vector? value) value
-        (or
-         (sequential? value)
-         (set? value))(into [] value)
-        :else (conj [] value)))
+  (if (coll? value)
+    (vec value)
+    (vector value)))
