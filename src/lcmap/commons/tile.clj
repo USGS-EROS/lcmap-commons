e4acd548d254c4631ecc7a052a39fba55f3017c1
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-01-13 11:55:37 -0600

    Explain tile ns function params

--- src/lcmap/commons/tile.clj
+++ src/lcmap/commons/tile.clj
@@ -15,15 +15,32 @@
   number (e.g. 30 * 100). Along the y-axis though, the pixel size and
   interval is a _negative_ value. Even though this may seem peculiar,
   using a negative value helps us avoid special cases for finding the
-  nearest tile-point."
+  nearest tile-point.
+
+  Parameters:
+  * point    - a scalar value on the real number line
+  * interval - a scalar value describing regularly spaced points
+               on the real number line"
   [point interval]
   (mod point interval))
 
 (defn near
   "Find nearest point given an interval and offset.
 
+  The nearest point will be lesser than the point for a positive
+  interval, and greater than the point for a negative interval as
+  is required when finding an 'upper-left' point on a cartesian
+  plane.
+
   This function is used to calculate the nearest points along the
-  x- and y- axis."
+  x- and y- axis.
+
+  Parameters:
+  * point    - a scalar value on the real number line
+  * interval - a scalar value describing regularly spaced points
+               on the real number line
+  * offset   - a scalar value used to shift point before and after
+               finding the 'preceding' interval."
   [point interval offset]
   (-> point
       (- offset)
@@ -48,7 +65,11 @@
   move from the origin of a raster file, the y-axis value _decreases_.
 
   The offset value is used for grids that are not aligned with the
-  origin of the projection coordinate system."
+  origin of the projection coordinate system.
+
+  * point     - a map containing :x and :y coordinates
+  * tile-spec - a map containing the interval and offset for x-
+                and y-axis"
   [{x :x y :y :as point}
    {:keys [x-interval y-interval x-offset y-offset] :as tile-spec}]
   {:x (near x x-interval x-offset)
@@ -56,9 +77,13 @@
 
 (defn snap
   "Transform an arbitrary projection system coordinate (x,y) into the
-   coordinate of the tile that contains it."
-  [x y spec]
-  (let [{:keys [tile_x tile_y shift_x shift_y]} spec
+   coordinate of the tile that contains it.
+
+  * x         - x coordinate
+  * y         - y coordinate
+  * tile-spec - parameters for a tile's grid system"
+  [x y tile-spec]
+  (let [{:keys [tile_x tile_y shift_x shift_y]} tile-spec
         tile (point->tile {:x x :y y}
                           {:x-interval tile_x
                            :y-interval tile_y
