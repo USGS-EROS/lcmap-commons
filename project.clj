046a8d41f52192da0ebbcd6695e1d542e59b2134
davidvhill <david.hill.ctr@usgs.gov>
2018-11-20 13:38:28 -0600

    remove prior artifacts from project.clj

--- project.clj
+++ project.clj
@@ -1,34 +1,8 @@
   (defproject gov.usgs.eros/lcmap-commons "1.0.1"
-   :parent-project {
-                    :coords [gov.usgs.eros/lcmap-system "1.0.0-SNAPSHOT"]
-                    :inherit [
-                              :deploy-repositories
-                              :license
-                              :managed-dependencies
-                              :plugins
-                              :pom-addition
-                              :repositories
-                              :target]}
-      ;; XXX The following can be un-commented once this issue is resolved:
-      ;;     * https://github.com/achin/lein-parent/issues/3
-      ;; [:profiles [:uberjar :dev]]
-
-   :description "LCMAP Commons"
    :url "https://github.com/USGS-EROS/lcmap-commons"
-   :dependencies [[org.clojure/clojure]
-                  [org.clojure/core.match]
-                  [org.clojure/data.codec]
-                  [org.clojure/data.json]
-                  [org.clojure/data.xml]
-                  [org.clojure/core.memoize]
-                   ;; Error Handling
-                  [dire]
-                  [slingshot]
-                   ;; Dev and project metadata
-                  [leiningen-core]
-                  ;; for lcmap-commons/time
-                  [clj-time/clj-time]]
-   :plugins [[lein-parent "0.3.0"]]
+    :dependencies [[clj-time/clj-time "0.15.0"]
+                   [org.clojure/clojure "1.9.0"]]
+                 
    :repl-options {:init-ns lcmap.commons.dev}
    :codox {
            :project {:name "lcmap.commons"
@@ -38,6 +12,17 @@
            :doc-paths ["docs/source"]
            :metadata {:doc/format :markdown
                       :doc "Documentation forthcoming"}}
+   :repositories [
+    ["maven" {
+      :url "https://oss.sonatype.org/content/repositories/snapshots/"
+      :creds :gpg}]]
+   :deploy-repositories [
+    ["releases" {
+      :url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+      :creds :gpg}
+     "snapshots" {
+      :url "https://oss.sonatype.org/content/repositories/snapshots/"
+      :creds :gpg}]]
    :profiles {
               :uberjar {:aot :all}
               ;; configuration for dev environment -- if you need to make local changes,
